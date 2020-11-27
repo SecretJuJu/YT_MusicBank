124b7eca4f7988a132213b0e5bf9c6194a9944ca
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2020-11-27 22:37:49 +0900

    [ADD] download function

--- app/Http/Controllers/YoutubeController.php
+++ app/Http/Controllers/YoutubeController.php
@@ -27,6 +27,17 @@ class YoutubeController extends Controller
             return view('youtube.search', ['retData'=>$retData]);
         }
 
+        // take info using youtube-dl
         return dd($_GET['uri']);
+
+        // 링크가 유효하다면 result : true 아니라면 result : false
+        
+
+        // true 일때 mp3 로 다운로드 할지 mp4로 다운로드할지 선택
+    }
+
+    public function download(Request $request)
+    {
+        return "it works!";
     }
 }
